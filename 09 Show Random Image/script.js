2e52b8c757f51696c5a373d354db64dac537ad68
Eric van Rees <ericvanrees@gmail.com>
2025-02-06 13:42:21 +0100

    added comment in script.js file

--- 09 Show Random Image/script.js
+++ 09 Show Random Image/script.js
@@ -3,6 +3,7 @@ let btn = document.querySelector(".btn");
 let imageDiv = document.querySelector('.img');
 
 btn.addEventListener("click", function() {
+  // insert new element instead of refreshing the index.html page
     imageDiv.innerHTML = 
         `<object
           data="https://picsum.photos/500/250"
